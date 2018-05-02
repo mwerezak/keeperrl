50d63213156122cefe0d6121293d63a76235eaf4
Michal Brzozowski <miki@extremic.com>
2018-05-02 14:18:13 +1000

    Fix logging

--- renderer.cpp
+++ renderer.cpp
@@ -245,10 +245,10 @@ static void renderArray(SDL::GLuint currentTexture,
     SDL::glBegin(GL_TRIANGLES);
     std::cout << "Triangle begin\n";
     for (int i = triangle * 3; i < (triangle + 1) * 3; ++i) {
-      std::cout << "Vertex " << vertices[2 * i] << vertices[2 * i + 1] << "\n";
       SDL::glColor4f(colors[4 * i], colors[4 * i + 1], colors[4 * i + 2], colors[4 * i + 3]);
       addTexCoord(Vec2(1, 1), texCoords[2 * i + 0], texCoords[2 * i + 1]);
-      std::cout << "Tex coord " << texCoords[2 * i + 0] << texCoords[2 * i + 1] << "\n";
+      std::cout << "Tex coord " << texCoords[2 * i + 0] << " " << texCoords[2 * i + 1] << "\n";
+      std::cout << "Vertex " << vertices[2 * i] << " " << vertices[2 * i + 1] << "\n";
       SDL::glVertex2f(vertices[2 * i], vertices[2 * i + 1]);
     }
     std::cout << "Triangle end\n";
